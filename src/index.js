ceab98b1db235d195e9d7721e729a398ee6c4e49
E Luckie <eluckie.d@gmail.com>
2023-02-10 11:46:41 -0700

    display conversion on DOM when form is submitted

--- src/index.js
+++ src/index.js
@@ -7,15 +7,18 @@ function getConversion(amount, fromUnit, toUnit) {
   CurrencyExchangeService.getConversion(amount, fromUnit, toUnit)
     .then(function(response) {
       if(response.result) {
-        displayConversion(response);
+        displayConversion(response, amount, fromUnit, toUnit);
       } else {
         displayError(response);
       }
     });
 }
 
-function displayConversion(response) {
-  return response;
+function displayConversion(response, amount, fromUnit, toUnit) {
+  let div = document.getElementById("conversion-display");
+  div.innerText = null;
+  div.innerText = `${amount} ${fromUnit} = ${response.conversion_result} ${toUnit}
+  The conversion rate is 1 ${fromUnit} : ${response.conversion_rate} ${toUnit}`;
 }
 
 function displayError(response) {
@@ -27,10 +30,8 @@ function formSubmission(event) {
   const amount = document.getElementById("conversion-amount").value;
   const fromUnit = document.getElementById("convert-from-unit").value;
   const toUnit = document.getElementById("convert-to-unit").value;
-  console.log("amount: " + amount);
-  console.log("fromUnit: " + fromUnit);
-  console.log("toUnit: " + toUnit);
   getConversion(amount, fromUnit, toUnit);
+  document.getElementById("currency-converter-form").reset();
 } 
 
 document.getElementById("currency-converter-form").addEventListener("submit", formSubmission);
--- src/js/CurrencyExchangeService.js
+++ src/js/CurrencyExchangeService.js
@@ -2,7 +2,6 @@ export default class CurrencyExchangeService {
   static getConversion(amount, fromUnit, toUnit) {
     return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${fromUnit}/${toUnit}/${amount}`)
       .then(function(response) {
-        console.log(response);
         if (!response.ok) {
           const errorMessage = `${response.result} ${response.error_type}`;
           throw new Error(errorMessage);
