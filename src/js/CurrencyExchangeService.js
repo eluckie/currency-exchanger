c9cd2ab71f0efee2992069b88f9b7b4881c5c1f6
E Luckie <eluckie.d@gmail.com>
2023-02-10 10:27:48 -0700

    add API call that uses static function

---
+++ src/js/CurrencyExchangeService.js
@@ -0,0 +1,16 @@
+export default class CurrencyExchangeService {
+  static getConversion(amount, fromUnit, toUnit) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${fromUnit}/${toUnit}/${amount}`)
+    .then(function(response) {
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText}: ${response.error-type}`;
+        throw new Error(errorMessage);
+      } else {
+        return response.json();
+      }
+    })
+    .catch(function(errorMessage) {
+      return errorMessage;
+    });
+  }
+}
